c29f0a97046b4a0fa564e301409c3833fc79c32e
soniarosem <157826405+soniarosem@users.noreply.github.com>
2025-02-17 23:57:37 +0000

    task 5 completed

--- wsgi.py
+++ wsgi.py
@@ -30,3 +30,16 @@ def get_users():
   # gets all objects of a model
   users = User.query.all()
   print(users)
+
+@app.cli.command("change-email")
+@click.argument('username', default='bob')
+@click.argument('email', default='bob@mail.com')
+def change_email(username, email):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  bob.email = email
+  db.session.add(bob)
+  db.session.commit()
+  print(bob)
